7d692d5d1a700eb652280ef3e181f7f1240ac741
onerealti <murtazaofficial@proton.me>
2026-07-04 18:15:45 +0530

    fix: specify absolute turbopack.root path in admin config

--- apps/admin/next.config.ts
+++ apps/admin/next.config.ts
@@ -1,11 +1,15 @@
 import { composePlugins, withNx } from '@nx/next';
 import type { NextConfig } from "next";
+import path from 'path';
 
 const nextConfig: NextConfig = {
   /* config options here */
   images:{
     domains:["lh3.googleusercontent.com"]
-  }
+  },
+  turbopack: {
+    root: path.resolve(__dirname, '../../'),
+  },
 };
 
 const plugins = [withNx];
